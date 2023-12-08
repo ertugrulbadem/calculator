2805b60523fbf3342fbf8be4b90ec42b540fc530
ertugrulbadem <40797902+ertugrulbadem@users.noreply.github.com>
2023-12-08 17:29:00 +0300

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+  export default{
+    data(){
+      return{
+        number1: null,
+        number2: null,
+        totalResult: null,
+      }
+    },
+    methods:{
+      collection(){
+        const collect = +this.number1 + +this.number2;
+        this.totalResult = collect;
+      },
+      division(){
+        const divis = this.number1 / this.number2;
+        this.totalResult = divis;
+      },
+      extraction(){
+        const extra = this.number1 - this.number2;
+        this.totalResult = extra;
+      },
+      multiplication(){
+        const multi = this.number1 * this.number2;
+        this.totalResult = multi;
+      }
+    }
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <v-container class="">
+      <p class="text-center">Hesap makinesi</p>
+      <p> {{ totalResult > 100 ? "100'den Büyük" : "100'den Küçük" }}</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+      <h1>{{ totalResult }}</h1>
+    <v-text-field v-model="number1"></v-text-field>
+    <v-text-field v-model="number2"></v-text-field>
+    <div >
+    <v-btn class="gap-5" @click="collection">Topla</v-btn>
+    <v-btn class="gap-5" @click="extraction">Çıkart</v-btn>
+    <v-btn class="gap-5" @click="division">Böl</v-btn>
+    <v-btn class="gap-5" @click="multiplication">Çarp</v-btn>
+  </div>
+  </v-container>
+  </div>
 </template>
 
 <style scoped>
@@ -22,6 +52,13 @@ header {
   line-height: 1.5;
 }
 
+.gap-5{
+  margin-right: 5px;
+}
+.button{
+  border: 1px solid black;
+  padding: 8px;
+}
 .logo {
   display: block;
   margin: 0 auto 2rem;
